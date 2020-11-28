3ccf160907a3e29116c168d08e5f5a8194b1f6df
Veena-S <veenasivan@gmail.com>
2020-11-28 17:16:43 +0800

    Updated for AWS deployment

--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
-CREATEDB birding;
+--CREATEDB birding;
 
 CREATE TABLE notes (id SERIAL PRIMARY KEY, -- index of entry
 noted_date_time TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP, -- the date and time, when the observation is made
@@ -12,14 +12,10 @@ species_id INTEGER
 
  CREATE TABLE users (id SERIAL PRIMARY KEY, email TEXT, password TEXT, username TEXT);
 
- ALTER TABLE notes ADD COLUMN user_id INTEGER;
-
  -- 3.PCE.7: Bird Watching Species
 
  CREATE TABLE species (id SERIAL PRIMARY KEY, name TEXT, scientific_name TEXT);
 
-
- ALTER TABLE notes ADD COLUMN species_id INTEGER;
 
  CREATE TABLE bird_behaviours (id SERIAL PRIMARY KEY, behaviour TEXT);
 
